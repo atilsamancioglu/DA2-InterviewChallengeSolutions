4646d50b5586dac1902898485099d932f47b4afb
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2022-05-08 22:03:57 +0300

    find the duplicate question added

--- findtheduplicatenumber.py
+++ findtheduplicatenumber.py
@@ -1,3 +1,12 @@
+'''
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+'''
+
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         #Floyd's Algorithm
