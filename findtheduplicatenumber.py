458e4afd45492add0594ef7be29168017ae97df8
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2022-05-08 21:26:54 +0300

    find the duplicate number completed

---
+++ findtheduplicatenumber.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        #Floyd's Algorithm
+        slowPointer = 0
+        fastPointer = 0
+        while True:
+            slowPointer = nums[slowPointer]
+            fastPointer = nums[nums[fastPointer]]
+            if slowPointer == fastPointer:
+                break
+        
+        secondSlowPointer = 0
+        
+        while True:
+            slowPointer = nums[slowPointer]
+            secondSlowPointer = nums[secondSlowPointer]
+            if slowPointer == secondSlowPointer:
+                return slowPointer
+        
+        
+        '''
+        O(n^2)
+        for i in range(0,len(nums)):
+            for j in range(0,len(nums)):
+                if i == j:
+                    continue
+                if nums[i] == nums[j]:
+                    return nums[i]
+        '''
