4d5f55a5a395fedc9850b4ac5f1b7c8e02dce06b
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2022-05-21 13:25:50 +0300

    minor revisions

--- binarytreemaximumpathsum.py
+++ binarytreemaximumpathsum.py
@@ -1,3 +1,12 @@
+'''
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+
+The path sum of a path is the sum of the node's values in the path.
+
+Given the root of a binary tree, return the maximum path sum of any non-empty path.
+
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -9,16 +18,19 @@ class Solution:
     answer = -float("inf")
     
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.recursiveSolution(root)
+        self.dfs(root)
         return self.answer
     
-    def recursiveSolution(self,node):
+    def dfs(self,node):
         if node is None:
             return 0
-        left = self.recursiveSolution(node.left) #we return maxSide because of this left & right
-        right = self.recursiveSolution(node.right)
         
-        maxSide = max(node.val, max(left,right) + node.val)
-        maxTop = max(maxSide, left+right+node.val)
-        self.answer = max(self.answer,maxTop)
-        return maxSide
+        left = self.dfs(node.left) 
+        right = self.dfs(node.right)
+        
+        left = max(left,0)
+        right = max(right,0)
+        
+        self.answer = max(self.answer,node.val+left+right)
+        
+        return node.val + max(left,right)
